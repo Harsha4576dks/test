153417544aeafa856fd949570b4191e0bfe29ce6
Harsha4576dks <harshivahm@gmail.com>
2026-07-03 20:09:21 +0530

    example

---
+++ school.py
@@ -0,0 +1,49 @@
+def school():
+    print("1: 1std class teacher:")
+    print("2: 2std class teacher:")
+    print("3: 3std class teacher:")
+    print("4: 4std class teacher:")
+    print("5: 5std class teacher:")
+    print("6: 6std class teacher:")
+    print("7: 7std class teacher:")
+    print("8: 8std class teacher:")
+    print("9: 9std class teacher:")
+    print("10: 10std class teacher:")
+    choice = int(input("enter your choice:"))
+
+    while True:
+        if choice == 1:
+            print("The class teacher is sumitra")
+            break
+        elif choice == 2:
+            print("The class teacher is sheela")
+            break
+        elif choice == 3:
+            print("The class teacher is suma")
+            break
+        elif choice == 4:
+            print("The class teacher is ramya")
+            break
+        elif choice == 5:
+            print("The class teacher is sowmya")
+            break
+        elif choice == 6:
+            print("The class teacher is kavitha")
+            break
+        elif choice == 7:
+            print("The class teacher is jyothi")
+            break
+        elif choice == 8:
+            print("The class teacher is bhavya")
+            break
+        elif choice ==  9:
+            print("The class teacher is veena")
+            break
+        elif choice == 10:
+            print("The class teacher is kajal")
+            break 
+        else:
+            print("the choice is inavlid")
+            exit()
+
+school()
