9f5d04e69b2edae00f85c406d4d5d8114c26b398
Harsha4576dks <harshivahm@gmail.com>
2026-07-16 21:27:58 +0530

    practice

--- reminder.py
+++ reminder.py
@@ -7,22 +7,23 @@ class Remainder():
     def __init__(self, tasks):
         self.tasks = tasks
 
-    def add_task(self, task, deadline, done):
+    def add_task(self, task, deadline, done=False):
         for t in self.tasks:
             if t["task"].lower() == task.lower():
                 print("This task is already scheduled")
                 return
-            else:
-                self.tasks.append({"task":task, "deadline":deadline, "done":done})
-                print(f"Task '{task} added successfully")   
+            
+        self.tasks.append({"task":task, "deadline":deadline, "done":done})
+        print(f"Task '{task} added successfully")   
 
 status = Remainder(Tasks)
 task = input("add new task:")
 deadline = input("add deadline(YYYY-MM-DD):")
-done = input("enter status:")
 
-status.add_task(task, deadline, done)
+status.add_task(task, deadline)
 
 print("current Tasks:")
 for t in Tasks:
     print(t)
+else:
+    exit()
